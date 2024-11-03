69d90eddd450dbe06fe055d957af4ba84ecc8d18
Anish Jain <Anish.Jain@cat.com>
2024-11-03 13:42:08 +0530

    2nd part completed - UI design

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,53 @@
+const Conversation = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+          />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+            <div className="flex gap-3 justify-between">
+                <p className="font-bold text-gray-200">Jon Snow</p>
+                <span className="text-xl">emoji</span>
+            </div>
+        </div>
+      </div>
+      <div className="divider my-0 py-0 h-1"></div>
+    </>
+  );
+};
+
+export default Conversation;
+
+// STARTER CODE
+// const Conversation = () => {
+//     return (
+//       <>
+//         <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+//           <div className="avatar online">
+//             <div className="w-12 rounded-full">
+//               <img
+//                 src="https://www.google.com/imgres?q=image&imgurl=https%3A%2F%2Fgratisography.com%2Fwp-content%2Fuploads%2F2024%2F10%2Fgratisography-cool-cat-800x525.jpg&imgrefurl=https%3A%2F%2Fgratisography.com%2F&docid=YAe2I9AqIHgndM&tbnid=JTZXQS5iqyP2NM&vet=12ahUKEwiE67-hyL-JAxUi-DgGHXv7CloQM3oECBgQAA..i&w=800&h=525&hcb=2&ved=2ahUKEwiE67-hyL-JAxUi-DgGHXv7CloQM3oECBgQAA"
+//                 alt="user avatar"
+//               />
+//             </div>
+//           </div>
+//           <div className="flex flex-col flex-1">
+//               <div className="flex gap-3 justify-between">
+//                   <p className="font-bold text-gray-200">Jon Snow</p>
+//                   <span className="text-xl">emoji</span>
+//               </div>
+//           </div>
+//         </div>
+//         <div className="divider my-0 py-0 h-1"></div>
+//       </>
+//     );
+//   };
+  
+//   export default Conversation;
+  
